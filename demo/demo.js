127e57024a3d8580739b81e74f28977171fa1ff2
phareal <akakpo.jeanjacques@gmail.com>
2016-12-30 08:41:46 +0000

    Flaterial DateTimePicker
    Dévelopment version released.
    
    @TODO: Clean code, Build client version

--- demo/demo.js
+++ demo/demo.js
@@ -1,12 +1,19 @@
 new FlaterialDateTimePicker(document.getElementById('dateInput'), {
 	minDate: null,
 	// minDate: "10-10-2016",
+	
 	maxDate: null,
 	// maxDate: "30-12-2018",
-	// currentDate: "31-10-2016",
+	
 	currentDate: null,
-	currentTime: "13:35",
+	// currentDate: "31-10-2016",
+	
+	currentTime: null,
+	// currentTime: "13:35",
+	
 	date: true,
+
 	time: true,
+	
 	format: "dddd DD MMMM YYYY - HH:mm"
 });
